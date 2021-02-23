1a17736d681ec238680a69c6a5041d01fe739962
Sisin-Hope <31354453+Sisin-Hope@users.noreply.github.com>
2021-02-23 16:45:35 +0100

    update by hassan dahbi

--- js/script.js
+++ js/script.js
@@ -1,28 +1,22 @@
 var menu = document.getElementById("menu");
-var presentation=document.getElementById("presentation");
+var presentation = document.getElementById("presentation");
 var _name = document.getElementById("name");
-// ---------------------------------
-
-
-
-function _click(){
-    change();
-
-}
-
-
 var question = document.querySelector(".qu");
 var answers = document.querySelector(".answers");
+var answer1 = document.querySelector(".answer1");
+var answer2 = document.querySelector(".answer2");
+var answer3 = document.querySelector(".answer3");
+var answer4 = document.querySelector(".answer4");
 var an1 = document.querySelector(".an1");
 var an2 = document.querySelector(".an2");
 var an3 = document.querySelector(".an3");
 var an4 = document.querySelector(".an4");
-
+// --------Style-------------------
+menu.style.visibility="hidden";
+// ---------------------------------
 var number_question=0;
 var time_left = 15;
 
-
-menu.style.visibility="hidden";
 
 // function for star
 document.querySelector(".btn-start").addEventListener("click",function(){
@@ -49,24 +43,19 @@ var downloadTimer = setInterval(function(){
     if(time_left <= -1){
       // clearInterval(downloadTimer);
     } else { 
-      document.querySelector(".count-down").innerHTML = time_left +" s";       
-      if(time_left <= 10){
-      document.querySelector(".count-down").style.background='red';
-      if(time_left == 0){
-        change();
-      }
-      }
-
+        document.querySelector(".count-down").innerHTML = time_left +" s";       
+        if(time_left <= 10){
+            document.querySelector(".count-down").style.background='red';
+            if(time_left == 0){
+                change();
+            }
+        }
     }
     time_left -= 1;
   }, 1000
 );
 
-
-
-
-
-
+// function for change question and answers
 function change(){
     number_question += 1;
     number_qe();
@@ -74,11 +63,12 @@ function change(){
     document.querySelector(".count-down").style.background='';
 }
 
+// function for question and answers
 function number_qe(){
     switch(number_question){
         case 1:
             question.innerHTML ="aaaaaaaaaaaaaaaaaa";
-            an1.innerHTML ="aaa";
+            an1.innerHTML ="true";
             an2.innerHTML ="aaa";
             an3.innerHTML ="aaa";
             an4.innerHTML ="aaa";
@@ -87,63 +77,63 @@ function number_qe(){
             question.innerHTML ="bbbbbbbbbbbbbbbbbbb";
             an1.innerHTML ="bbb";
             an2.innerHTML ="bbb";
-            an3.innerHTML ="bbb";
+            an3.innerHTML ="true";
             an4.innerHTML ="bbb";
         break;
         case 3:
             question.innerHTML ="ccccccccccccccccccc";
             an1.innerHTML ="ccc";
-            an2.innerHTML ="ccc";
+            an2.innerHTML ="true";
             an3.innerHTML ="ccc";
             an4.innerHTML ="ccc";
         break;
         case 4:
             question.innerHTML ="eeeeeeeeeeeeeeeeeeee";
-            an1.innerHTML ="aaa";
-            an2.innerHTML ="aaa";
-            an3.innerHTML ="aaa";
-            an4.innerHTML ="aaa";
+            an1.innerHTML ="eee";
+            an2.innerHTML ="eee";
+            an3.innerHTML ="eee";
+            an4.innerHTML ="true";
         break;
         case 5:
             question.innerHTML ="ffffffffffffffffffff";
-            an1.innerHTML ="bbb";
-            an2.innerHTML ="bbb";
-            an3.innerHTML ="bbb";
-            an4.innerHTML ="bbb";
+            an1.innerHTML ="fff";
+            an2.innerHTML ="fff";
+            an3.innerHTML ="fff";
+            an4.innerHTML ="true";
         break;
         case 6:
             question.innerHTML ="ggggggggggggggggggggg";
-            an1.innerHTML ="ccc";
-            an2.innerHTML ="ccc";
-            an3.innerHTML ="ccc";
-            an4.innerHTML ="ccc";
+            an1.innerHTML ="ggg";
+            an2.innerHTML ="true";
+            an3.innerHTML ="ggg";
+            an4.innerHTML ="ggg";
         break;
         case 7:
             question.innerHTML ="hhhhhhhhhhhhhhhhhhhhhhhhh";
-            an1.innerHTML ="aaa";
-            an2.innerHTML ="aaa";
-            an3.innerHTML ="aaa";
-            an4.innerHTML ="aaa";
+            an1.innerHTML ="true";
+            an2.innerHTML ="hhh";
+            an3.innerHTML ="hhh";
+            an4.innerHTML ="hhh";
         break;
         case 8:
             question.innerHTML ="iiiiiiiiiiiiiiiiiiiiiiiiiiiii";
-            an1.innerHTML ="bbb";
-            an2.innerHTML ="bbb";
-            an3.innerHTML ="bbb";
-            an4.innerHTML ="bbb";
+            an1.innerHTML ="iii";
+            an2.innerHTML ="iii";
+            an3.innerHTML ="true";
+            an4.innerHTML ="iii";
         break;
         case 9:
             question.innerHTML ="xxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-            an1.innerHTML ="ccc";
-            an2.innerHTML ="ccc";
-            an3.innerHTML ="ccc";
-            an4.innerHTML ="ccc";
+            an1.innerHTML ="true";
+            an2.innerHTML ="xxx";
+            an3.innerHTML ="xxx";
+            an4.innerHTML ="xxx";
         break;
         default:
             question.innerHTML ="wwwwwwwwwwwwwwwwwwwwwwwwwww";
-            an1.innerHTML ="ddd";
-            an2.innerHTML ="ddd";
-            an3.innerHTML ="ddd";
-            an4.innerHTML ="ddd";
+            an1.innerHTML ="www";
+            an2.innerHTML ="true";
+            an3.innerHTML ="www";
+            an4.innerHTML ="www";
     }
 }
