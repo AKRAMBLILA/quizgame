78a347622244e6e08daddcb3cf75e68bf37969a0
Sisin-Hope <31354453+Sisin-Hope@users.noreply.github.com>
2021-02-24 01:42:13 +0100

    update by hassan dahbi (sisin hope)

--- js/script.js
+++ js/script.js
@@ -11,39 +11,37 @@ var an1 = document.querySelector(".an1");
 var an2 = document.querySelector(".an2");
 var an3 = document.querySelector(".an3");
 var an4 = document.querySelector(".an4");
-// --------Style-------------------
-menu.style.visibility="hidden";
+var _audio = new Audio("/audio/game.mp3");
 // ---------------------------------
 var q=0;
-var number_question=0;
-var time_left = 15;
+var time_left = 50;
 var win=document.querySelector(".win");
 var lose=document.querySelector(".win");
-var swi=Math.floor(Math.random()*5)+1;
+var is_correct=true;
+// --------Style-------------------
+menu.style.visibility="hidden";
 win.style.visibility='hidden';
 
 // function for star
 document.querySelector(".btn-start").addEventListener("click",function(){
-
     if(_name.value === ""){
         alert("enter your name !!");
     }else{
-        time_left = 15;7
+        _audio.play();
+        time_left = 50;
         presentation.style.visibility="hidden";
         menu.style.visibility="visible";
-        document.querySelector(".count-down").style.background='';
-        questionchange();
+        document.querySelector(".count-down").style.background='';       
+        question_change();
     }
 });
-
 // function for exit
 document.querySelector(".leave").addEventListener("click",function(){
     presentation.style.visibility="visible";
     menu.style.visibility="hidden";
-    time_left = 15;
+    time_left = 50;
     document.querySelector(".count-down").style.background='';
 });
-
 // function for timer
 var downloadTimer = setInterval(function(){
     if(time_left <= -1){
@@ -52,6 +50,7 @@ var downloadTimer = setInterval(function(){
         document.querySelector(".count-down").innerHTML = time_left +" s";       
         if(time_left <= 10){
             document.querySelector(".count-down").style.background='red';
+            
             if(time_left == 0){
                 win.style.visibility='visible';
                 lose.innerHTML='You Lose ! you can try again ';
@@ -61,113 +60,62 @@ var downloadTimer = setInterval(function(){
     time_left -= 1;
   }, 1000
 );
-
 // function for change question and answers
-function change(){
-    number_question += 1;
-    number_qe();
-    time_left = 15;
-    document.querySelector(".count-down").style.background='';
+function question_change(){
+    if(is_correct ===true){
+        q++;
+        document.getElementById("sc"+q).style.backgroundColor='#314e14';
+        if(q>=2){
+            document.getElementById("sc"+(q-1)).style.backgroundColor='';
+        }
+        if(q<=3 && q>0)
+        {
+            easyQus();
+        }else if(q<=7 && q>3)
+        {
+            moyQus();
+        }else if(q<=10 && q>7){
+            hardQus();
+        }
+        time_left = 50;
+        document.querySelector(".count-down").style.background='';
+        is_correct=false;
+        answer1.style.backgroundColor='';
+        answer2.style.backgroundColor='';
+        answer3.style.backgroundColor='';
+        answer4.style.backgroundColor='';
+
+    }else{
+        presentation.style.visibility="visible";
+        menu.style.visibility="hidden";
+        time_left = 50;
+        document.querySelector(".count-down").style.background='';
+ 
+    }
 }
-
-
 // function for question and answers
-function number_qe(){
-    switch(number_question){
-        case 1:
-            question.innerHTML ="aaaaaaaaaaaaaaaaaa";
-            an1.innerHTML ="true";
-            an2.innerHTML ="aaa";
-            an3.innerHTML ="aaa";
-            an4.innerHTML ="aaa";
-        break;
-        case 2:
-            question.innerHTML ="bbbbbbbbbbbbbbbbbbb";
-            an1.innerHTML ="bbb";
-            an2.innerHTML ="bbb";
-            an3.innerHTML ="true";
-            an4.innerHTML ="bbb";
-        break;
-        case 3:
-            question.innerHTML ="ccccccccccccccccccc";
-            an1.innerHTML ="ccc";
-            an2.innerHTML ="true";
-            an3.innerHTML ="ccc";
-            an4.innerHTML ="ccc";
-        break;
-        case 4:
-            question.innerHTML ="eeeeeeeeeeeeeeeeeeee";
-            an1.innerHTML ="eee";
-            an2.innerHTML ="eee";
-            an3.innerHTML ="eee";
-            an4.innerHTML ="true";
-        break;
-        case 5:
-            question.innerHTML ="ffffffffffffffffffff";
-            an1.innerHTML ="fff";
-            an2.innerHTML ="fff";
-            an3.innerHTML ="fff";
-            an4.innerHTML ="true";
-        break;
-        case 6:
-            question.innerHTML ="ggggggggggggggggggggg";
-            an1.innerHTML ="ggg";
-            an2.innerHTML ="true";
-            an3.innerHTML ="ggg";
-            an4.innerHTML ="ggg";
-        break;
-        case 7:
-            question.innerHTML ="hhhhhhhhhhhhhhhhhhhhhhhhh";
-            an1.innerHTML ="true";
-            an2.innerHTML ="hhh";
-            an3.innerHTML ="hhh";
-            an4.innerHTML ="hhh";
-        break;
-        case 8:
-            question.innerHTML ="iiiiiiiiiiiiiiiiiiiiiiiiiiiii";
-            an1.innerHTML ="iii";
-            an2.innerHTML ="iii";
-            an3.innerHTML ="true";
-            an4.innerHTML ="iii";
-        break;
-        case 9:
-            question.innerHTML ="xxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-            an1.innerHTML ="true";
-            an2.innerHTML ="xxx";
-            an3.innerHTML ="xxx";
-            an4.innerHTML ="xxx";
-        break;
-        default:
-            question.innerHTML ="wwwwwwwwwwwwwwwwwwwwwwwwwww";
-            an1.innerHTML ="www";
-            an2.innerHTML ="true";
-            an3.innerHTML ="www";
-            an4.innerHTML ="www";
-    }
+function sa7i7(){
+    is_correct=true;
 }
-
-function questionchange(){
-    q++;
-    document.getElementById("sc"+q).style.backgroundColor='#314e14';
-    document.getElementById("sc"+(q-1)).style.background='';
-    if(q<=3 && q>0)
-    {
-        easyQus();
-    }else if(q<=7 && q>3)
-    {
-        moyQus();
-    }else if(q<=10 && q>7){
-        hardQus();
-    }
-    
-    time_left = 15;
-    document.querySelector(".count-down").style.background='';
-    
+function yes(){
+    answer1.style.backgroundColor='';
+    answer2.style.backgroundColor='';
+    answer3.style.backgroundColor='';
+    answer4.style.backgroundColor='';
+    this.style.backgroundColor='green';
 }
-
+function _event(){
+    answer1.addEventListener("click",yes);
+    answer2.addEventListener("click",yes);
+    answer3.addEventListener("click",yes);
+    answer4.addEventListener("click",yes);
+}
 function easyQus(){
+    swi=Math.floor(Math.random()*5)+1;
     switch(swi){
         case 1:
+            answer1.addEventListener("click",sa7i7);
+            _event();
             question.innerHTML ="easyaaaaaaaaaaaaaaaaaa";
             an1.innerHTML ="true";
             an2.innerHTML ="aaa";
@@ -175,6 +123,8 @@ function easyQus(){
             an4.innerHTML ="aaa";
         break;
         case 2:
+            answer3.addEventListener("click",sa7i7);
+            _event();
             question.innerHTML ="easybbbbbbbbbbbbbbbbbbb";
             an1.innerHTML ="bbb";
             an2.innerHTML ="bbb";
@@ -182,6 +132,8 @@ function easyQus(){
             an4.innerHTML ="bbb";
         break;
         case 3:
+            answer2.addEventListener("click",sa7i7);
+            _event();
             question.innerHTML ="easyccccccccccccccccccc";
             an1.innerHTML ="ccc";
             an2.innerHTML ="true";
@@ -189,6 +141,8 @@ function easyQus(){
             an4.innerHTML ="ccc";
         break;
         case 4:
+            answer4.addEventListener("click",sa7i7);
+            _event();
             question.innerHTML ="easyeeeeeeeeeeeeeeeeeeee";
             an1.innerHTML ="eee";
             an2.innerHTML ="eee";
@@ -196,6 +150,8 @@ function easyQus(){
             an4.innerHTML ="true";
         break;
         case 5:
+            answer4.addEventListener("click",sa7i7);
+            _event();
             question.innerHTML ="easyffffffffffffffffffff";
             an1.innerHTML ="fff";
             an2.innerHTML ="fff";
@@ -204,10 +160,12 @@ function easyQus(){
         break;
     }
 }
-
 function moyQus(){
+    swi=Math.floor(Math.random()*5)+1;
     switch(swi){
         case 1:
+            answer1.addEventListener("click",sa7i7);
+            _event();
             question.innerHTML ="moyaaaaaaaaaaaaaaaaaa";
             an1.innerHTML ="true";
             an2.innerHTML ="aaa";
@@ -215,6 +173,8 @@ function moyQus(){
             an4.innerHTML ="aaa";
         break;
         case 2:
+            answer3.addEventListener("click",sa7i7);
+            _event();
             question.innerHTML ="moybbbbbbbbbbbbbbbbbbb";
             an1.innerHTML ="bbb";
             an2.innerHTML ="bbb";
@@ -222,6 +182,8 @@ function moyQus(){
             an4.innerHTML ="bbb";
         break;
         case 3:
+            answer2.addEventListener("click",sa7i7);
+            _event();
             question.innerHTML ="moyccccccccccccccccccc";
             an1.innerHTML ="ccc";
             an2.innerHTML ="true";
@@ -229,6 +191,8 @@ function moyQus(){
             an4.innerHTML ="ccc";
         break;
         case 4:
+            answer4.addEventListener("click",sa7i7);
+            _event();
             question.innerHTML ="moyeeeeeeeeeeeeeeeeeeee";
             an1.innerHTML ="eee";
             an2.innerHTML ="eee";
@@ -236,6 +200,8 @@ function moyQus(){
             an4.innerHTML ="true";
         break;
         case 5:
+            answer4.addEventListener("click",sa7i7);
+            _event();
             question.innerHTML ="moyffffffffffffffffffff";
             an1.innerHTML ="fff";
             an2.innerHTML ="fff";
@@ -244,10 +210,12 @@ function moyQus(){
         break;
     }
 }
-
 function hardQus(){
+    swi=Math.floor(Math.random()*5)+1;
     switch(swi){
         case 1:
+            answer1.addEventListener("click",sa7i7);
+            _event();
             question.innerHTML ="hardaaaaaaaaaaaaaaaaaa";
             an1.innerHTML ="true";
             an2.innerHTML ="aaa";
@@ -255,6 +223,8 @@ function hardQus(){
             an4.innerHTML ="aaa";
         break;
         case 2:
+            answer3.addEventListener("click",sa7i7);
+            _event();
             question.innerHTML ="hardbbbbbbbbbbbbbbbbbbb";
             an1.innerHTML ="bbb";
             an2.innerHTML ="bbb";
@@ -262,6 +232,8 @@ function hardQus(){
             an4.innerHTML ="bbb";
         break;
         case 3:
+            answer2.addEventListener("click",sa7i7);
+            _event();
             question.innerHTML ="hardccccccccccccccccccc";
             an1.innerHTML ="ccc";
             an2.innerHTML ="true";
@@ -269,6 +241,8 @@ function hardQus(){
             an4.innerHTML ="ccc";
         break;
         case 4:
+            answer4.addEventListener("click",sa7i7);
+            _event();
             question.innerHTML ="hardeeeeeeeeeeeeeeeeeeee";
             an1.innerHTML ="eee";
             an2.innerHTML ="eee";
@@ -276,6 +250,8 @@ function hardQus(){
             an4.innerHTML ="true";
         break;
         case 5:
+            answer4.addEventListener("click",sa7i7);
+            _event();
             question.innerHTML ="hardffffffffffffffffffff";
             an1.innerHTML ="fff";
             an2.innerHTML ="fff";
